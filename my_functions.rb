3b7ede6ca000089c68b41fde7d381d7ee462bae5
Rameez <rameez.t@live.com>
2018-07-18 14:50:22 +0100

    Sum of arrays

--- my_functions.rb
+++ my_functions.rb
@@ -1,7 +1,17 @@
 
 
 def add_array_lengths(array_1, array_2)
+  return array_1.length() + array_2.length()
+end
 
-return array_1.length() + array_2.length()
+def sum_array(numbers)
+
+  sum_array = 0
+
+  for num in numbers
+
+  sum_array += num
+  end
+return sum_array
 
 end
